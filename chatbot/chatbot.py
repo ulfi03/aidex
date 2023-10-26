92d44c8ea2f8d3de2498c7f6ea4cf7176f58323b
risky <n11werthmann@gmail.com>
2023-10-26 14:18:09 +0200

    Anpassungen & weitergehende Implementation

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,29 +1,28 @@
 import openai
 import os
 
-#os.environ['REQUESTS_CA_BUNDLE'] = './cert/Corp-Prj-Root-CA.crt'          # for corp proxy
-os.environ['REQUESTS_CA_BUNDLE'] = './cert/Baltimore CyberTrust Root.crt' # for home proxy
+os.environ['REQUESTS_CA_BUNDLE'] = './cert/Corp-Prj-Root-CA.crt'          # for corp proxy
+#os.environ['REQUESTS_CA_BUNDLE'] = './cert/Baltimore CyberTrust Root.crt' # for home proxy
 openai.api_key = "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"
 
 nachrichten = [
-    {"role": "system", "content": "Du wandelst Fließtexte in Karteikarten um"}
+    {"role": "system", "content": "Du bist ein Konverter: Du wandelst Fließtexte in Karteikarten um"}
   ]
+while True:
+    nutzerEingabe = input("Stelle eine Frage: ")
+    nachrichten.append({"role": "user", "content": nutzerEingabe})
 
-nutzerEingabe = input("Stelle eine Frage: ")
-nachrichten.append({"role": "user", "content": nutzerEingabe})
-
-completion = openai.ChatCompletion.create( #with history
-  model = "gpt-3.5-turbo",
-  temperature = 0.0, 
-  max_tokens = 100, 
-  messages = nachrichten
-)
-response = openai.Completion.create( #no history
-  model="gpt-3.5-turbo-instruct",
-  prompt = nutzerEingabe,
-  max_tokens=100,
-  temperature=0
-)
-
-print(completion['choices'][0].message.content)
-print(response['choices'][0].text)
+    completion = openai.ChatCompletion.create( #with history
+      model = "gpt-3.5-turbo",
+      temperature = 0.0, 
+      max_tokens = 100, 
+      messages = nachrichten
+    )
+    response = openai.Completion.create( #no history
+      model="gpt-3.5-turbo-instruct",
+      prompt = nutzerEingabe,
+      max_tokens=100,
+      temperature=0
+    )
+    print("with history - "+ completion['choices'][0].message.content)
+    print("no history - " + response['choices'][0].text.strip())
