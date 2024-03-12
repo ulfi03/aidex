d8716d8213bb2d0ecf0a4bfeb4ef4e1f7bbece72
risky <n11werthmann@gmail.com>
2024-03-12 19:01:39 +0100

    Cleaned up Repo and removed sample project -> ready for implementation of the overview

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
-import 'package:aidex/screens/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flashcards App',
-        home: HomePage());
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+      ),
+    );
   }
 }
