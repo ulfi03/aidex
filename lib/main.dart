a913d59d9a47a4e1315af3584179347d4054a603
ulfi03 <marius.ulfers@outlook.de>
2024-03-20 10:14:37 +0100

    #51 finished rich-text-editor widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:aidex/ui/quill-poc/quill_editor_widget.dart';
+import 'package:aidex/ui/deck-overview/deck_overview_widget.dart';
+import 'package:aidex/ui/theme/aidex_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 /// The main application widget.
@@ -10,64 +11,12 @@ void main() {
 /// This widget is the root of the application.
 class MyApp extends StatelessWidget {
   /// Constructor for the main application widget.
-  MyApp({super.key});
-
-  final QuillEditorWidget _quillEditorWidget = QuillEditorWidget();
+  const MyApp({super.key});
 
   /// The key for the QuillEditorWidget.
   @override
-  Widget build(final BuildContext context) {
-    //_quillEditorWidget.controller.document.toDelta().toJson();
-    _quillEditorWidget.controller.document.insert(0, 'Hello World!');
-    return MaterialApp(
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color(0xFF20EFC0),
-            primary: const Color(0xFF20EFC0),
-            onPrimary: Colors.black,
-            background: Colors.black,
-            onBackground: Colors.white,
-            surface: const Color(0xFF121212).withOpacity(0.9),
-            onSurface: Colors.white,
-            onSurfaceVariant: Colors.white38,
-
-          ),
-          textTheme:
-            const TextTheme(
-              displayLarge: TextStyle(color: Colors.white),
-              displayMedium: TextStyle(color: Colors.white),
-              displaySmall: TextStyle(color: Colors.white),
-              bodyLarge: TextStyle(color: Colors.white),
-              bodyMedium: TextStyle(color: Colors.white),
-              bodySmall: TextStyle(color: Colors.white),
-              labelLarge: TextStyle(color: Colors.white),
-              labelMedium: TextStyle(color: Colors.white),
-              labelSmall: TextStyle(color: Colors.white),
-              headlineLarge: TextStyle(color: Colors.white),
-              headlineMedium: TextStyle(color: Colors.white),
-              headlineSmall: TextStyle(color: Colors.white),
-              titleLarge: TextStyle(color: Colors.white),
-              titleMedium: TextStyle(color: Colors.white),
-              titleSmall: TextStyle(color: Colors.white),
-            ),
-          // change icon color to blue
-        ),
-        //home: DeckOverviewWidget(),
-        home: Scaffold(
-          resizeToAvoidBottomInset: true,
-          appBar: AppBar(
-            centerTitle: true,
-            title: const Text(
-              'Create Index Card',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            backgroundColor: const Color(0xFF121212),
-          ),
-          body: _quillEditorWidget,
-        ));
-  }
+  Widget build(final BuildContext context) => MaterialApp(
+        theme: mainTheme,
+        home: const DeckOverviewWidget(),
+      );
 }
--- lib/ui/components/rich-text-editor/rich_text_editor_widget.dart
+++ lib/ui/components/rich-text-editor/rich_text_editor_widget.dart
@@ -1,14 +1,23 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:flutter_quill_extensions/flutter_quill_embeds.dart';
 
 /// Rich text editor widget.
-class QuillEditorWidget extends StatelessWidget {
-  /// Constructor for the [QuillEditorWidget].
-  QuillEditorWidget({super.key});
+class RichTextEditorWidget extends StatelessWidget {
+  /// Constructor for the [RichTextEditorWidget].
+  RichTextEditorWidget({super.key});
 
   /// The controller for the quill editor.
-  final controller = QuillController.basic();
+  final _controller = QuillController.basic();
+
+  /// Converts the content of the editor to JSON.
+  String toJson() => jsonEncode(_controller.document.toDelta().toJson());
+
+  /// Inserts the content described by the passed [json] in the editor.
+  void fromJson(final String json) =>
+      _controller.document = Document.fromJson(jsonDecode(json));
 
   @override
   Widget build(final BuildContext context) => Flex(
@@ -16,18 +25,19 @@ class QuillEditorWidget extends StatelessWidget {
         children: [
           QuillToolbar.simple(
             configurations: QuillSimpleToolbarConfigurations(
-              controller: controller,
+              controller: _controller,
               multiRowsDisplay: false,
               toolbarSize: 40,
-              sharedConfigurations:
-              const QuillSharedConfigurations(locale: Locale('de'),),
+              sharedConfigurations: const QuillSharedConfigurations(
+                locale: Locale('de'),
+              ),
               embedButtons: FlutterQuillEmbeds.toolbarButtons(),
             ),
           ),
           Expanded(
             child: QuillEditor.basic(
               configurations: QuillEditorConfigurations(
-                controller: controller,
+                controller: _controller,
                 sharedConfigurations: const QuillSharedConfigurations(
                   locale: Locale('de'),
                 ),
@@ -35,8 +45,6 @@ class QuillEditorWidget extends StatelessWidget {
               ),
             ),
           ),
-
-
         ],
       );
 }
