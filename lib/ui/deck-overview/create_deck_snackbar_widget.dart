e45f39174535bc748571ee3ab46d1b3cb70ab88e
ulfi03 <marius.ulfers@outlook.de>
2024-04-27 23:20:11 +0200

    #87 Converted CreateDeckSnackbar to ModalBottomSheet

--- lib/ui/deck-overview/create_deck_snackbar_widget.dart
+++ lib/ui/deck-overview/create_deck_snackbar_widget.dart
@@ -1,38 +1,5 @@
 import 'package:aidex/ui/theme/aidex_theme.dart';
 import 'package:flutter/material.dart';
-/// A widget used to display the Snackbar for creating a deck.
-class CreateDeckSnackbar extends SnackBar {
-
-  /// Constructor for the [CreateDeckSnackbar].
-  CreateDeckSnackbar({required final VoidCallback onManual,
-    required final VoidCallback onAI, super.key})
-      : super(
-    backgroundColor: mainTheme.colorScheme.background,
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(20),
-        topRight: Radius.circular(20),
-      ),
-    ),
-    duration: const Duration(days: 365),
-    content: _CreateDeckSnackbarWidget(
-      onManual: onManual,
-      onAI: onAI,
-    ),
-  );
-
-  /// The key for the Snackbar title
-  static const snackbarTitleKey = Key('create_deck_snackbar_title');
-
-  /// The key for the create manually title
-  static const createManuallyTitleKey = Key('create_manually_title');
-
-  /// The key for the create AI title
-  static const createAITitleKey = Key('create_ai_title');
-
-  /// The key for the create manually button
-  static const createManuallyButtonKey = Key('create_manually_button');
-}
 
 /// A widget used to display the Snackbar for creating a deck.
 ///
@@ -40,7 +7,7 @@ class CreateDeckSnackbar extends SnackBar {
 ///
 /// It displays two buttons for the user to create a deck manually or with AI.
 ///
-/// The [_CreateDeckSnackbarWidget] requires a [onManual] and [onAI] callback
+/// The [CreateDeckSnackbarWidget] requires a [onManual] and [onAI] callback
 /// to be provided.
 ///
 /// The [onManual] callback is called when the user presses the button to create
@@ -68,8 +35,8 @@ class CreateDeckSnackbar extends SnackBar {
 /// ```
 ///
 /// {@category Widget}
-class _CreateDeckSnackbarWidget extends StatelessWidget {
-  /// Constructor for the [_CreateDeckSnackbarWidget].
+class CreateDeckSnackbarWidget extends StatelessWidget {
+  /// Constructor for the [CreateDeckSnackbarWidget].
   ///
   /// The [key] is used to identify the widget in the widget tree.
   ///
@@ -78,8 +45,20 @@ class _CreateDeckSnackbarWidget extends StatelessWidget {
   ///
   /// The [onAI] callback is called when the user presses the button to create
   /// a deck with AI.
-  const _CreateDeckSnackbarWidget(
-      {required this.onManual, required this.onAI});
+  const CreateDeckSnackbarWidget(
+      {required this.onManual, required this.onAI, super.key});
+
+  /// The key for the Snackbar title
+  static const snackbarTitleKey = Key('create_deck_snackbar_title');
+
+  /// The key for the create manually title
+  static const createManuallyTitleKey = Key('create_manually_title');
+
+  /// The key for the create AI title
+  static const createAITitleKey = Key('create_ai_title');
+
+  /// The key for the create manually button
+  static const createManuallyButtonKey = Key('create_manually_button');
 
   /// The callback called when the user presses the button to create a deck
   /// manually
@@ -90,63 +69,58 @@ class _CreateDeckSnackbarWidget extends StatelessWidget {
   final VoidCallback onAI;
 
   @override
-  Widget build(final BuildContext context) =>
-      Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: Column(
-                children: [
-                  Text(
-                    key: CreateDeckSnackbar.snackbarTitleKey,
-                    'Create Deck',
-                    style: mainTheme.textTheme.titleLarge
-                  ),
-                ],
+  Widget build(final BuildContext context) => Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Center(
+              child: Text(
+                  key: snackbarTitleKey,
+                  'Create Deck',
+                  style: mainTheme.textTheme.titleLarge),
+            ),
+            ElevatedButton.icon(
+              key: createManuallyButtonKey,
+              onPressed: onManual,
+              icon: Icon(
+                Icons.person,
+                color: mainTheme.colorScheme.primary,
+              ),
+              label: Text(
+                  key: createManuallyTitleKey,
+                  'Create manually',
+                  style: mainTheme.textTheme.bodyMedium),
+              style: ElevatedButton.styleFrom(
+                alignment: Alignment.centerLeft,
+                backgroundColor: Colors.transparent,
+                shadowColor: Colors.transparent,
+                padding: EdgeInsets.zero,
+                elevation: 0,
               ),
             ),
-          ),
-          ElevatedButton.icon(
-            key: CreateDeckSnackbar.createManuallyButtonKey,
-            onPressed: onManual,
-            icon: Icon(
-              Icons.person,
-              color: mainTheme.colorScheme.primary,
-            ),
-            label: Text(
-              key: CreateDeckSnackbar.createManuallyTitleKey,
-              'Create manually',
-              style: mainTheme.textTheme.bodySmall
-            ),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.transparent,
-              shadowColor: Colors.transparent,
-              padding: EdgeInsets.zero,
-              elevation: 0,
-            ),
-          ),
-          const SizedBox(height: 10),
-          ElevatedButton.icon(
-            onPressed: onAI,
-            icon: Icon(
-              Icons.smart_toy,
-              color: mainTheme.colorScheme.primary,
-            ),
-            label: Text(
-              key: CreateDeckSnackbar.createAITitleKey,
-              'Create with AI',
-              style: mainTheme.textTheme.bodySmall
-            ),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.transparent,
-              shadowColor: Colors.transparent,
-              padding: EdgeInsets.zero,
-              elevation: 0,
-            ),
-          ),
-        ],
+            ElevatedButton.icon(
+              onPressed: onAI,
+              icon: Icon(
+                Icons.smart_toy,
+                color: mainTheme.colorScheme.primary,
+              ),
+              label: Text(
+                key: createAITitleKey,
+                'Create with AI',
+                style: mainTheme.textTheme.bodyMedium,
+              ),
+              style: ElevatedButton.styleFrom(
+                alignment: Alignment.centerLeft,
+                backgroundColor: Colors.transparent,
+                shadowColor: Colors.transparent,
+                padding: EdgeInsets.zero,
+                elevation: 0,
+              ),
+            )
+          ],
+        ),
       );
 }
